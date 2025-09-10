4ff21fcded42b6ebb8905e9c9ab7da32bf337bd0
tech1savvy <amankumar010604@gmail.com>
2025-09-10 10:16:41 +0530

    Add thanks URL to exam/urls.py

--- myproject/exam/urls.py
+++ myproject/exam/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import feedback_view
+from .views import feedback_view, thanks_view
 
 urlpatterns = [
     path('feedback/', feedback_view, name='feedback'),
+    path('thanks/', thanks_view, name='thanks'),
 ]
