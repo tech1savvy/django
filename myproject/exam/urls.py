a899dbda42cc4a1a36afb5b9c60557d1a19726f1
tech1savvy <amankumar010604@gmail.com>
2025-09-10 10:20:36 +0530

    Add register URL to exam/urls.py

--- myproject/exam/urls.py
+++ myproject/exam/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import feedback_view, thanks_view
+from .views import feedback_view, thanks_view, register_view
 
 urlpatterns = [
     path('feedback/', feedback_view, name='feedback'),
     path('thanks/', thanks_view, name='thanks'),
+    path('register/', register_view, name='register'),
 ]
