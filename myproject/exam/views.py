0c9a3b0cd0985be38c6d594ff6a8471424f73991
tech1savvy <amankumar010604@gmail.com>
2025-09-10 10:15:43 +0530

    Add feedback_view to exam/views.py

--- myproject/exam/views.py
+++ myproject/exam/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import FeedbackForm
+from .models import Feedback
 
-# Create your views here.
+@login_required
+def feedback_view(request):
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            Feedback.objects.create(
+                name=form.cleaned_data['name'],
+                email=form.cleaned_data['email'],
+                rating=form.cleaned_data['rating'],
+                comments=form.cleaned_data['comments']
+            )
+            return redirect('thanks')  # or wherever you want after submission
+    else:
+        form = FeedbackForm()
+    return render(request, 'feedback.html', {'form': form})
