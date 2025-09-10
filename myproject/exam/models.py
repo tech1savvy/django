314a9ff38fb38e12e7ae3e272ee753ee3c909baa
tech1savvy <amankumar010604@gmail.com>
2025-09-10 10:15:17 +0530

    Add Feedback model to exam/models.py

--- myproject/exam/models.py
+++ myproject/exam/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    rating = models.PositiveSmallIntegerField()  # 1 to 5
+    comments = models.TextField(blank=True)
+    submitted_at = models.DateTimeField(auto_now_add=True)
